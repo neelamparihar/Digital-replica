b0be8fd10fa00609e6af625a6aedc0238523f214
Neelam <pariharneelam376@gmail.com>
2025-01-21 14:22:12 +0530

    test

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,18 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-require("dotenv").config(); // Load environment variables
+require("dotenv").config();
 const cors = require("cors");
 const replicaRoutes = require("./routes/Replica");
 const path = require("path");
 
-app.use(express.static(path.join(__dirname, "build"))); 
 const app = express();
+
 app.use(cors());
-// Middleware
 app.use(bodyParser.json());
 
 // MongoDB Connection
-const mongoURI = process.env.MONGODB_URI; // Fetch URI from .env
+const mongoURI = process.env.MONGODB_URI;
 if (!mongoURI) {
   console.error("Error: MONGODB_URI is not defined in .env");
   process.exit(1);
@@ -24,19 +23,16 @@ mongoose
   .then(() => console.log("MongoDB connected successfully"))
   .catch((err) => {
     console.error("MongoDB connection error:", err.message);
-    process.exit(1); // Exit process if connection fails
+    process.exit(1);
   });
 
-// Routes
 app.use("/api/replicas", replicaRoutes);
 
-// Error handling middleware
 app.use((err, req, res, next) => {
   console.error("Unhandled Error:", err);
   res.status(500).json({ message: "Internal Server Error" });
 });
 
-// Start the server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
